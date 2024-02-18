fabd821b7df681fe91af0adaec2dd305425ea975
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-02-18 01:11:29 -0500

    UsapView

--- UserApp/views.py
+++ UserApp/views.py
@@ -14,10 +14,6 @@ def create_profile(request):
     try:
         user_profile = UserProfile.objects.get(user=request.user)
     except UserProfile.DoesNotExist:
-<<<<<<< HEAD
-        # Si aucun UserProfile n'existe, en créer un pour l'utilisateur actuel
-=======
->>>>>>> 275d7765e9dd1b476bacfce6d6bfc918eb07bad2
         user_profile = UserProfile(user=request.user)
         user_profile.save()
         
@@ -37,10 +33,7 @@ def edit_profile(request):
     try:
         user_profile = UserProfile.objects.get(user=request.user)
     except UserProfile.DoesNotExist:
-<<<<<<< HEAD
-        # Si aucun UserProfile n'existe, en créer un pour l'utilisateur actuel
-=======
->>>>>>> 275d7765e9dd1b476bacfce6d6bfc918eb07bad2
+
         user_profile = UserProfile(user=request.user)
         user_profile.save()
 
